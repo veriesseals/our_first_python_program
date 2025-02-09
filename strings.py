3c56794d80603860075a332efad49c2142fdd22c
Veries Seals <veries@me.com>
2025-02-09 11:12:01 -0600

    Print Function

---
+++ strings.py
@@ -0,0 +1,32 @@
+print("Today is a good day to learn Python")
+
+print("Python is fun")
+
+print("Python's string are easy to use")
+
+print("We can even include 'quotes' in strings")
+
+# -----------------------------------------------------------
+
+# String Concatination (Join)
+print("hello" + "world")
+# -----------------------------------------------------------
+
+# We can store Strings inside varibles
+
+greeting = "Hello"
+
+# name = "Veries"
+# -----------------------------------------------------------
+
+# Create an input as a varible using the input function. It will allow the end user to enter their name
+name = input ("Please enter your name ")
+
+# -----------------------------------------------------------
+# Call Print function with using the varible names. It will output the value of the variable.
+# Greating without the space
+
+print(greeting  + name)
+# Greating with the space
+print (greeting + ' ' + name)
+
